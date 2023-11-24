04a09fd22de0d2f390976b7b15c2193aeb9bf9b8
zyrouge <zyrouge@hotmail.com>
2023-11-24 20:58:53 +0530

    chore: typo

--- utils/tasks.ts
+++ utils/tasks.ts
@@ -1,9 +1,9 @@
 export const handleTask = async <T>(name: string, task: () => Promise<T>) => {
     try {
         const result = await task();
-        console.log(`Excution of '${name}' was successful!`);
+        console.log(`Execution of '${name}' succeeded!`);
         return result;
     } catch (error) {
-        console.error(`Excution of '${name}' was failed! Reason: ${error}.`);
+        console.error(`Execution of '${name}' failed! (${error})`);
     }
 };
